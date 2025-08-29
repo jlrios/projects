e7c93fb64f1593e2af68bf72fa94c7e23a184016
José Ríos <rdjl5791@gmail.com>
2025-08-29 01:05:44 -0600

    Starting the development of the text adventure game: Crisium

---
+++ text-based-games/crisium/crisium.py
@@ -0,0 +1,38 @@
+currentRoom = "Common Area"
+
+inventory = []
+
+rooms = {
+    "Hall": {
+        "south": "Common Area"
+    },
+    "Common Area": {
+        "north": "Hall",
+        "item": "card",
+        "itemType": "access"
+    } 
+} 
+
+print("You are in the " + currentRoom)
+
+def showInstructions():
+    print("CRISIUM GAME")
+    print("==============")
+    print("Go [direction]")
+    print("Get [item]")
+
+while True:
+    showInstructions()
+
+    command = input("> ")
+    action = command.split(" ", 1)
+    if action[0] == "go":
+        if acction[1] in rooms[currentRoom]:
+            currentRoom = rooms[currentRoom][action[1]]
+            print("You are in the " + currentRoom)
+
+    if action[0] == "get":
+        if action[1] == rooms[currentRoom]["item"]:
+            print(f"You got a {action[1]}!")
+            inventory.append(action[1])
+            print("Inventory:", inventory)
